d02495c8bb6f3e4ea5a630d2ea6f478925027db5
Andy Ha <48343637+thienanstudios@users.noreply.github.com>
2020-02-21 17:49:22 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch,Route } from "react-router-dom";
+// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details"
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6"><span><i className="fas fa-home"></i></span></div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
