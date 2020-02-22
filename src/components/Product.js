797dd96717135e609d96a27496887bb98b804514
Andy Ha <48343637+thienanstudios@users.noreply.github.com>
2020-02-21 21:02:05 -0800

    cart but

--- src/components/Product.js
+++ src/components/Product.js
@@ -52,7 +52,7 @@ const ProductWrapper = styled.div`
 
   &:hover {
     .card {
-      border: 0.04 rem solid rgba(0,0,0, 0.2);
+      border: 0.04rem solid rgba(0,0,0, 0.2);
       box-shadow: 2px 2px 5px 0px rgba(0,0,0,0,2);
     }
     .card-footer {
@@ -71,4 +71,29 @@ const ProductWrapper = styled.div`
   .img-container: hover .card-img-top {
     transform: scale(1.2);
   }
+
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 0.5s linear;
+  }
+
+  .img-container: hover .cart-btn {
+    transform: translate(0,0);
+  }
+
+  .cart-btn:hover {
+    color: var(--mainBlue);
+    cursor: pointer;
+  }
 `;
+
+// 2:28
