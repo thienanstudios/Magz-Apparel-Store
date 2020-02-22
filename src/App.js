fa0f0a8f7db05654f6093670586e9634fa9823a8
Andy Ha <48343637+thienanstudios@users.noreply.github.com>
2020-02-21 18:01:20 -0800

    navbar incoming

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <React.Fragment>
       <Navbar />
       <Switch>
-        <Route path="/" component={ProductList} />
+        <Route exact path="/" component={ProductList} />
         <Route path="/details" component={Details} />
         <Route path="/cart" component={Cart} />
         <Route component={Default} />
