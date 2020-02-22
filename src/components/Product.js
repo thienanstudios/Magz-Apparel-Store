ae64255a7e2ea91a268b5728b9dc5778a9cdbab7
Andy Ha <48343637+thienanstudios@users.noreply.github.com>
2020-02-21 20:53:04 -0800

    zoom

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,11 +10,28 @@ export default class Product extends Component {
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
           {/* can change padding of image here "p-5" or p-0 */}
-          <div className="img-container p-0.8" onClick={() => console.log("You clciked me on img container")}>
+          <div className="img-container p-0" onClick={() => console.log("You clciked me on img container")}>
             <Link to="/details">
               <img src={img} alt="product" className="card-img-top" />
             </Link>
-            
+            <button className="cart-btn" disabled={inCart ? true : false} onClick={() => { console.log("added to cart") }} >
+              {inCart ? (<p className="text-capitalize mb-0" disabled>
+                in cart
+            </p>) : (<i className="fas fa-cart-plus" />)}
+            </button>
+
+          </div>
+          {/* card footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">
+              {title}
+            </p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">
+                $
+              </span>
+              {price}
+            </h5>
           </div>
         </div>
       </ProductWrapper>
@@ -23,5 +40,31 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
+  .card{
+    border-color: transparent;
+    transtion: all 1s linear;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+  }
 
-`
+  &:hover {
+    .card {
+      border: 0.04 rem solid rgba(0,0,0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0,0,0,0,2);
+    }
+    .card-footer {
+      background: rgba(247,247,247);
+    }
+  }
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .img-container: hover .card-img-top {
+    transform: scale(1.2);
+  }
+`;
