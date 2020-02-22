afef3982e35af8e3e0f89e5bc19d4d22efbbd52a
Andy Ha <48343637+thienanstudios@users.noreply.github.com>
2020-02-21 18:46:20 -0800

    colors

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,16 +5,16 @@ export const BUttonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4rem;
   background: transparent;
-  border: 0.1rem solid var(--lightBlue); 
-  color: var(--lightBlue);
+  border: 0.1rem solid var(--mainBlue); 
+  color: var(--mainBlue);
   border-radius: 0.5rem;
   padding: 0.2rem 0.5rem;
   cursor: pointer;
   margine: 0.2rem 0.5rem 0.2rem 0;
   transition: all 0.2s ease-in-out;
   &: hover{
-    background: var(--lightBlue);
-    color: var(--mainBlue);
+    background: var(--mainBlue);
+    color: var(--lightBlue);
   }
   &: focus{
     outline: none;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import { BUttonContainer } from './Button';
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expnd-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expnd-sm navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
           Creative Commons (Attribution 3.0 Unported);
           https://www.iconfinder.com/Makoto_msk 
@@ -31,8 +31,15 @@ export default class Navbar extends Component {
             my cart
           </BUttonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
 
+const NavWrapper = styled.nav`
+  background: var(--mainMagz);
+  .nav-link{
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+  }
+`
