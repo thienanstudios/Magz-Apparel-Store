519fda2eabc6bbdcf69412b765408debe39c3195
Andy Ha <48343637+thienanstudios@users.noreply.github.com>
2020-02-23 04:20:43 -0800

    cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Title from '../Title';
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import { ProductConsumer } from '../../context';
+
+
 
 export default class Cart extends Component {
   render() {
     return (
-      <div>
-        <h3> hello from carrt</h3>
-      </div>
+      <section>
+        <ProductConsumer>
+          {val => {
+            const { cart } = val;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="" title="cart" />
+                  <CartColumns />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
     )
   }
 }
